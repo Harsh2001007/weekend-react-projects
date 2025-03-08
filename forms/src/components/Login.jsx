dcd67e215ab22c7816b3ede6d9f0b874ff15e1cb
Harsh <hs5686584@gmail.com>
2025-03-08 22:25:11 +0530

    updated form data

--- forms/src/components/Login.jsx
+++ forms/src/components/Login.jsx
@@ -1,17 +1,31 @@
+import { useRef } from "react";
+
 export default function Login() {
+  const emailRef = useRef();
+  const passRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+
+    const enteredEmail = emailRef.current.value;
+    const enteredPass = passRef.current.value;
+
+    console.log(enteredEmail, enteredPass);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={emailRef} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={passRef} />
         </div>
       </div>
 
